b3b53d96039042eaaca6aef86fb10e8c45026207
Troy Melhase <troy@monster.ax>
2023-11-18 10:49:55 -0300

    first part of example.

---
+++ example/Program.cs
@@ -0,0 +1,42 @@
+﻿using Vements.API;
+
+class Program
+{
+    static string ExampleApiKey = "";
+    static string ExampleProjectId = "";
+
+    static void Main(string[] args)
+    {
+        var client = new Client(ExampleApiKey);
+
+        var participantCreateResponse = client.participant.Create(
+            new ParticipantCreateRequest(
+                projectId: ExampleProjectId,
+                externalId: "player-1",
+                display: "Player 12",
+                image: "",
+                extra: null
+            )
+        );
+
+        var participant = participantCreateResponse?.insertParticipantOne;
+        Console.WriteLine("New Participant Created: " + participant);
+
+        var scoreboardCreateResponse = client.scoreboard.Create(
+            new ScoreboardCreateRequest(
+                projectId: ExampleProjectId,
+                display: "Basically Exciting Scoreboard 2",
+                rankDir: "asc",
+                public_: true,
+                extra: null
+            )
+        );
+
+        var scoreboard = scoreboardCreateResponse?.insertScoreboardOne;
+        Console.WriteLine("New Scoreboard Created: " + scoreboard);
+
+        // Teardown
+        var scoreboardDeleteResponse = client.scoreboard.Delete(scoreboard?.scoreboardId);
+        Console.WriteLine("New Scoreboard Deleted: " + scoreboardDeleteResponse);
+    }
+}
--- example/readme.md
+++ example/readme.md
@@ -1,4 +1,19 @@
 ## C# Example for Vements
 
-This directory contains an example of how to use the Vements Client Library for C# to create a new Vements client, and then use that client to create a new Vements scoreboard, participant, and scores.
+This directory contains an example of how to use the Vements Client Library for C# to create a client and use that client to create a new Vements scoreboard, participant, and scores.
 
+
+To run the example:
+
+```shell
+$ git clone https://github.com/vements/client-cs
+$ cd client-cs/example
+$ dotnet run
+```
+
+This example was initialized with the following commands; you do not need to run these, they are here for reference only.
+
+```shell
+$ dotnet new console
+$ dotnet add package Vements.API
+```
